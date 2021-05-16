1692870d7fc24088730356218200599df4adc041
Sreedhar k.s <53270409+mellofordev@users.noreply.github.com>
2021-05-16 12:08:49 +0530

    Sigmoid function

---
+++ sigmoid.py
@@ -0,0 +1,11 @@
+import numpy as np #install numpy using pip install numpy
+
+def sigmoid(x):
+  return 1-(1+np.exp(-x)) #sigmoid function formula
+  
+sigmoid(x) #pass in the value for x
+
+
+  
+ 
+  
